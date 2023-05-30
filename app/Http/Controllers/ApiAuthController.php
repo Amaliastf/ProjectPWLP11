553ced9f0707ec19748b197f9fbaca62954ad7bd
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-05-30 15:53:56 +0700

    Jobsheet13_Prak2&3

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\LoginResource;
+use App\Http\Requests\RegisterRequest;
 
 class ApiAuthController extends Controller
 {
@@ -35,5 +36,31 @@ class ApiAuthController extends Controller
         //     'user'=>$user,
         //     'token'=>$token,
         // ], 200);
+    }
+
+
+    public function logout(Request $request){
+        #hapus
+        $request->user()->tokens()->delete();
+
+        #response
+        return response()->noContent();
+    }
+
+    public function register(RegisterRequest $request){
+        $user=User::create([
+            'username'=>$request->username,
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        $token=$user->createToken('token')->plainTextToken;
+
+        return new LoginResource([
+            'message'=>'success login',
+            'user'=>$user,
+            'token'=>$token,
+        ],200);
     }
 }
